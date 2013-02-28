5c3c3350d82d3e34f56c2a50b3fb171b9c67ed1d
TJ Holowaychuk <tj@vision-media.ca>
2013-02-28 14:12:23 -0800

    misc

--- test.js
+++ test.js
@@ -3,18 +3,18 @@ var Client = require('./');
 var client = new Client(process.argv[2]);
 var fs = require('fs');
 
-// client.url('http://i.cloudup.com/g5rLSt.xls', function(err, res){
-//   if (err) throw err;
-//   console.log(res);
-
-//   client.status(res.uuid, function(err, status){
-//     if (err) throw err;
-//     console.log(status);
-//   });
-// });
-
-client.thumb('a805033b-6043-4687-9639-b8fa7fd3c076', 300, 300, function(err, buf){
+client.url('http://i.cloudup.com/g5rLSt.xls', function(err, res){
   if (err) throw err;
-  console.log('write ./out.png');
-  fs.writeFile('out.png', buf);
+  console.log(res);
+
+  client.status(res.uuid, function(err, status){
+    if (err) throw err;
+    console.log(status);
+  });
 });
+
+// client.thumb('a805033b-6043-4687-9639-b8fa7fd3c076', 300, 300, function(err, buf){
+//   if (err) throw err;
+//   console.log('write ./out.png');
+//   fs.writeFile('out.png', buf);
+// });
